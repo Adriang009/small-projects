a52e463ab84f730a370fa3db7ab33d4e902e207c
Adriang009 <adriangiurgiu1@gmail.com>
2022-09-08 08:17:53 -0500

    adding small projects

---
+++ choose_your_own_adv/choose_your_own_adventure.py
@@ -0,0 +1,21 @@
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input("You are on a dirt road, it has come to an end and you can go left or right. Which way you'd like to go? ").lower()
+
+if answer == "left":
+    answer = input(
+        "You come to a river, you can walk around it or swim accross. Type walk to walk around and swim to swim across ")
+
+    if answer == "swim":
+        print("You swam accross and were eate by an alligator. ")
+    elif answer == "walk":
+        print('You walked for many miles, ran out of water and lost the game.')
+    else:
+        print("Not a valid option. You lose. ")
+        
+elif answer == "right":
+    print("")
+
+else:
+    print("Not a valid option. You lose. ")
---
+++ pass_manager/quiz_game.py
@@ -0,0 +1,42 @@
+from tkinter import Y
+
+
+print("Welcome to my Quiz")
+
+playing = input("Do you want to Play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorect!")
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorect!")
+print("You got " + str(score) + " questions correct!")
+print("That's " + str(score/4 * 100) + "%.")
